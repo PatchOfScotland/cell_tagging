cd391a6dd79becccbf0808de992055e8230425c8
Patch of Scotland <d.marchant@ed-alumni.net>
2018-09-17 10:50:19 +0200

    added in a extra button as I think that might actually be a better approach

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,58 @@
 import {
+  IDisposable, DisposableDelegate
+} from '@phosphor/disposable';
+
+import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
+import {
+  ToolbarButton
+} from '@jupyterlab/apputils';
+
+import {
+  DocumentRegistry
+} from '@jupyterlab/docregistry';
+
+import {
+  NotebookActions, NotebookPanel, INotebookModel
+} from '@jupyterlab/notebook';
+
 import '../style/index.css';
 
+// A notebook widget extension that adds a button to the toolbar
+export
+class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+  // Create a new extension object.
+  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+    let callback = () => {
+      NotebookActions.runAll(panel.content, context.session);
+    };
+    let button = new ToolbarButton({
+      className: 'myButton',
+      iconClassName: 'fa fa-fast-forward',
+      onClick: callback,
+      tooltip: 'Run All'
+    });
 
-/**
- * Initialization data for the jupyterlab_cell_tagging extension.
- */
-const extension: JupyterLabPlugin<void> = {
-  id: 'jupyterlab_cell_tagging',
-  autoStart: true,
-  activate: (app: JupyterLab) => {
-    console.log('JupyterLab extension jupyterlab_cell_tagging is activated!');
+    panel.toolbar.insertItem(0, 'runAll', button);
+    return new DisposableDelegate(() => {
+      button.dispose();
+    });
   }
+}
+
+// Activate the extension
+function activate(app: JupyterLab) {
+	console.log('JupyterLab extension jupyterlab_cell_tagging is activated with an extra button!');
+	app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
 };
 
-export default extension;
+// Initialization data for the jupyterlab_cell_tagging extension.
+const plugin: JupyterLabPlugin<void> = {
+  activate,
+  id: 'jupyterlab_cell_tagging',
+  autoStart: true
+};
+
+export default plugin;
