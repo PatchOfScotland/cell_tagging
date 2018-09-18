7ec958e06b5fe96f169ff7447a0939246dd86d12
Patch of Scotland <d.marchant@ed-alumni.net>
2018-09-18 10:56:43 +0200

    Identified the CellType variable in nbformat.ts

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,10 @@ import {
   NotebookActions, NotebookPanel, INotebookModel
 } from '@jupyterlab/notebook';
 
+import {
+		nbformat
+} from '@jupyterlab/coreutils';
+
 import '../style/index.css';
 
 // A notebook widget extension that adds a button to the toolbar
@@ -27,6 +31,19 @@ class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel
   createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
     let callback = () => {
       NotebookActions.runAll(panel.content, context.session);
+	  console.log('within callback');
+//	  let selectBox = panel.toolbar.node.childNodes[9];
+	  let selectBox = panel.toolbar.node.childNodes[9].childNodes[0].childNodes[0];
+
+	  let cellTypes = ['Task', 'Rules']
+	  for (let c of cellTypes) {
+		let option = document.createElement('option');
+		option.value = c.toLowerCase();
+		option.textContent = c;
+		selectBox.appendChild(option);
+      }
+	  console.log('got element for select box: ');
+	  console.log(selectBox);
     };
     let button = new ToolbarButton({
       className: 'myButton',
@@ -39,14 +56,28 @@ class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel
     return new DisposableDelegate(() => {
       button.dispose();
     });
+	
+	console.log('about to try and select:');
+//	let selectBox = panel.toolbar;
+//	console.log('got element for select box:');
+//	console.log(selectBox);
   }
 }
 
 // Activate the extension
 function activate(app: JupyterLab) {
 	console.log('JupyterLab extension jupyterlab_cell_tagging is activated with an extra button!');
+	console.log('testing that lab has updated');	
 	app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
+	
+	
+//	console.log(CellType);	
 };
+
+// Almost certainly incorrect
+export namespace nbformat {
+	export type CellType = 'code' | 'markdown' | 'raw' | 'task' | 'rule';
+}
 
 // Initialization data for the jupyterlab_cell_tagging extension.
 const plugin: JupyterLabPlugin<void> = {
